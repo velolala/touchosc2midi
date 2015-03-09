2b9b36b704c5b93c18c14f9de6ab8bca70cd6bd8
velolala <fiets@einstueckheilewelt.de>
2015-03-09 20:53:03 +0100

    Update MANIFEST, add classifiers

--- setup.py
+++ setup.py
@@ -18,5 +18,14 @@ setup(name='touchosc2midi',
       entry_points={"console_scripts": [
           "touchosc2midi = touchosc2midi.touchosc2midi:main"
                 ]
-            }
+            },
+      classifiers=[
+          "Development Status :: 4 - Beta",
+          "Environment :: Console",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: POSIX :: Linux",
+          "Topic :: Artistic Software",
+          "Topic :: Home Automation",
+          "Topic :: Multimedia :: Sound/Audio :: MIDI",
+      ]
       )
